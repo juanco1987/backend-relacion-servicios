99f6e1651c0ae3dbed47fd6392b58c225306fd64
juanco1987 <juancarvajal310@gmail.com>
2025-09-20 14:07:23 -0500

    ajuste referencias para el pdf en los init.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,7 @@
-from .excel_processor import extraer_servicios
-from .pdf_generator import generar_pdf, generar_pdf_modular, _abrir_pdf
+from .pdf_generator import generar_pdf, generar_pdf_modular
 
 __all__ = [
     'extraer_servicios',
     'generar_pdf',
-    'generar_pdf_modular',
-    '_abrir_pdf'
+    'generar_pdf_modular'
 ]
